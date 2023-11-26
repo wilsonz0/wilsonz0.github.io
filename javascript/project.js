d7b5de4ea046ce9f5d61b845e74d7f4b5aabaffa
wilsonz0 <105890511+wilsonz0@users.noreply.github.com>
2023-11-26 15:13:43 +0900

    add fours projects onto the page

--- javascript/project.js
+++ javascript/project.js
@@ -2,6 +2,3 @@ import { toggleContent } from "./common.js"
 
 console.log("Project.js is running")
 
-document.getElementById('portfolioProjectBTN').addEventListener("click", function() {
-    toggleContent("portfolioProjectBTN", "portfolioProject")
-});
